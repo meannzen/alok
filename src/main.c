d699b2373f930800dc878ede312b89562964eee8
meannzen <senmeann@gmail.com>
2026-05-28 14:14:39 +0700

    Handle invalid commands

--- src/main.c
+++ src/main.c
@@ -1,11 +1,46 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+  char *buffer;
+  size_t buffer_len;
+  size_t input_len;
+} InputBuffer;
+
+InputBuffer *new_input_buffer() {
+  InputBuffer *input = (InputBuffer *)malloc(sizeof(InputBuffer));
+  if (!input) {
+    exit(1);
+  }
+
+  input->buffer = NULL;
+  input->buffer_len = 0;
+  input->input_len = 0;
+
+  return input;
+}
+
+void free_input_buffer(InputBuffer *input) {
+  free(input->buffer);
+  free(input);
+}
+
+void read_input(InputBuffer *input) {
+  size_t byte_read = getline(&(input->buffer), &(input->buffer_len), stdin);
+  input->input_len = byte_read - 1;
+  input->buffer[byte_read - 1] = 0;
+}
+
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  for (;;) {
+    printf("$ ");
+    InputBuffer *input = new_input_buffer();
+    read_input(input);
+    printf("%s: command not found\n", input->buffer);
+    free_input_buffer(input);
+  }
 
   return 0;
 }
