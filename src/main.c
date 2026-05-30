78d0f2e6b3df30b051c578af65b83e38d349c531
meannzen <senmeann@gmail.com>
2026-05-30 13:37:45 +0700

    refactor

--- src/main.c
+++ src/main.c
@@ -3,58 +3,58 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-  char *buffer;
-  size_t buffer_len;
-  size_t input_len;
-} InputBuffer;
+struct input_buffer {
+        char *buffer;
+        size_t buffer_len;
+        size_t input_len;
+};
 
-InputBuffer *new_input_buffer() {
-  InputBuffer *input = (InputBuffer *)malloc(sizeof(InputBuffer));
-  if (!input) {
-    exit(1);
-  }
+struct input_buffer *new_input_buffer() {
+        struct input_buffer *input = (struct input_buffer *)malloc(sizeof(struct input_buffer));
 
-  input->buffer = NULL;
-  input->buffer_len = 0;
-  input->input_len = 0;
+        if (!input) {
+                exit(1);
+         }
 
-  return input;
+        input->buffer = NULL;
+        input->buffer_len = 0;
+        input->input_len = 0;
+
+        return input;
 }
 
-void free_input_buffer(InputBuffer *input) {
-  free(input->buffer);
-  free(input);
+void free_input_buffer(struct input_buffer *input) {
+        free(input->buffer);
+        free(input);
 }
 
-void read_input(InputBuffer *input) {
-  ssize_t byte_read = getline(&(input->buffer), &(input->buffer_len), stdin);
-  if (byte_read == -1) {
-    free_input_buffer(input);
-    exit(1);
-  }
-  input->input_len = byte_read - 1;
-  input->buffer[byte_read - 1] = '\0';
+void read_input(struct input_buffer *input) {
+        ssize_t byte_read = getline(&(input->buffer), &(input->buffer_len), stdin);
+
+        if (byte_read == -1) {
+                 free_input_buffer(input);
+                 exit(1);
+         }
+
+        input->input_len = byte_read - 1;
+        input->buffer[byte_read - 1] = '\0';
 }
 
 int main(int argc, char *argv[]) {
-  setbuf(stdout, NULL);
+        setbuf(stdout, NULL);
+        struct input_buffer *input = new_input_buffer();
 
-  InputBuffer *input = new_input_buffer();
-  for (;;) {
-    printf("$ ");
-    read_input(input);
-    if (input->input_len == 0)
-      continue;
+        while (1) {
+                 printf("$ ");
+                 read_input(input);
+                if (input->input_len == 0) continue;
 
-    if (strcmp(input->buffer, "exit") == 0) {
-      break;
-    }
+                if (strcmp(input->buffer, "exit") == 0) break;
 
-    printf("%s: command not found\n", input->buffer);
-  }
+                printf("%s: command not found\n", input->buffer);
+        }
 
-  free_input_buffer(input);
+        free_input_buffer(input);
 
-  return 0;
+        return 0;
 }
