2636dc43a9f9dca07f59488d671a94634dd7176b
meannzen <senmeann@gmail.com>
2026-07-06 10:44:09 +0700

    suck refector

--- src/main.c
+++ src/main.c
@@ -2,80 +2,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct input_buffer {
-  char *buffer;
-  size_t buffer_len;
-  size_t input_len;
+	char *buffer;
+	size_t buffer_len;
+	size_t input_len;
 };
 
-struct input_buffer *new_input_buffer() {
-  struct input_buffer *input =
-      (struct input_buffer *)malloc(sizeof(struct input_buffer));
+struct input_buffer *new_input_buffer()
+{
+	struct input_buffer *input =
+		(struct input_buffer *)malloc(sizeof(struct input_buffer));
 
-  if (!input) {
-    exit(1);
-  }
+	if (!input) {
+		exit(1);
+	}
 
-  input->buffer = NULL;
-  input->buffer_len = 0;
-  input->input_len = 0;
+	input->buffer = NULL;
+	input->buffer_len = 0;
+	input->input_len = 0;
 
-  return input;
+	return input;
 }
 
-void free_input_buffer(struct input_buffer *input) {
-  free(input->buffer);
-  free(input);
+void free_input_buffer(struct input_buffer *input)
+{
+	free(input->buffer);
+	free(input);
 }
 
-void read_input(struct input_buffer *input) {
-  ssize_t byte_read = getline(&(input->buffer), &(input->buffer_len), stdin);
+void read_input(struct input_buffer *input)
+{
+	ssize_t byte_read =
+		getline(&(input->buffer), &(input->buffer_len), stdin);
 
-  if (byte_read == -1) {
-    free_input_buffer(input);
-    exit(1);
-  }
+	if (byte_read == -1) {
+		free_input_buffer(input);
+		exit(1);
+	}
 
-  input->input_len = byte_read - 1;
-  input->buffer[byte_read - 1] = '\0';
+	input->input_len = byte_read - 1;
+	input->buffer[byte_read - 1] = '\0';
 }
 
-int main(int argc, char *argv[]) {
-  setbuf(stdout, NULL);
-  struct input_buffer *input = new_input_buffer();
+int main(int argc, char *argv[])
+{
+	setbuf(stdout, NULL);
+	struct input_buffer *input = new_input_buffer();
 
-  while (1) {
-    printf("$ ");
-    read_input(input);
-    if (input->input_len == 0)
-      continue;
+	const char *BUILT_IN[] = { "exit", "type", "echo" };
 
-    if (strcmp(input->buffer, "exit") == 0)
-      break;
+	const size_t build_len = sizeof(BUILT_IN) / sizeof(size_t);
 
-    if(strncmp(input->buffer, "echo ", 5)== 0) {
-	  printf("%s\n", input->buffer + 5);
-	  continue;
-    }
+	while (1) {
+		printf("$ ");
+		read_input(input);
+		if (input->input_len == 0)
+			continue;
 
-    if(strncmp(input->buffer, "type ", 5)== 0)  {
-	    if(strcmp(input->buffer + 5, "exit") == 0 
-			    || strcmp(input->buffer + 5, "echo") == 0 
-			    || strcmp(input->buffer + 5, "type") == 0) {
+		if (strcmp(input->buffer, "exit") == 0)
+			break;
 
-		    printf("%s is a shell builtin\n", input->buffer + 5);
-	    } else {
-                printf("%s: not found\n", input->buffer + 5);
-	    }
+		if (strncmp(input->buffer, "echo ", 5) == 0) {
+			printf("%s\n", input->buffer + 5);
+			continue;
+		}
 
-	   continue;
-    }
+		if (strncmp(input->buffer, "type ", 5) == 0) {
+			bool found_build_in = false;
+			for (size_t i = 0; i < build_len; i++) {
+				if (strcmp(input->buffer + 5, BUILT_IN[i]) ==
+				    0) {
+					found_build_in = true;
+					printf("%s is a shell builtin\n",
+					       input->buffer + 5);
+					break;
+				}
+			}
+			if (!found_build_in) {
+				printf("%s: not found\n", input->buffer + 5);
+			}
+			continue;
+		}
 
-    printf("%s: command not found\n", input->buffer);
-  }
+		printf("%s: command not found\n", input->buffer);
+	}
 
-  free_input_buffer(input);
+	free_input_buffer(input);
 
-  return 0;
+	return 0;
 }
